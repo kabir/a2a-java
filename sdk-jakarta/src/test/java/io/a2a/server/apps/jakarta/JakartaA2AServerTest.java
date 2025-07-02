9c085b0a524688d50bda7974bf38f0d27d9e8b4d
Emmanuel Hugonnet <ehugonne@redhat.com>
2025-07-02 13:20:44 +0200

    Usin WildFly to test the Jakarta SDK
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- sdk-jakarta/src/test/java/io/a2a/server/apps/jakarta/JakartaA2AServerTest.java
+++ sdk-jakarta/src/test/java/io/a2a/server/apps/jakarta/JakartaA2AServerTest.java
@@ -1,14 +1,42 @@
 package io.a2a.server.apps.jakarta;
 
+
 import jakarta.inject.Inject;
 
 import io.a2a.server.apps.common.AbstractA2AServerTest;
+import org.jboss.arquillian.junit5.ArquillianExtension;
 import io.a2a.server.events.InMemoryQueueManager;
 import io.a2a.server.tasks.TaskStore;
-import io.quarkus.test.junit.QuarkusTest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@QuarkusTest
+@ExtendWith(ArquillianExtension.class)
 public class JakartaA2AServerTest extends AbstractA2AServerTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() throws IOException {
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, "test.war")
+//                .addPackages(true, "io.a2a.server")
+//                .addPackages(true, "io.a2a.spec")
+//                .addPackages(true, "io.a2a.util")
+//                .addPackages(true, "io.a2a.client")
+//                .addPackages(true, "io.a2a.http")
+                .addClass(A2AServerResource.class)
+                .addClass(A2ARequestFilter.class)
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        archive.as(ZipExporter.class).exportTo(Files.newOutputStream(Paths.get("test.war"), StandardOpenOption.CREATE));
+        return archive;
+    }
+    
     @Inject
     TaskStore taskStore;
 
